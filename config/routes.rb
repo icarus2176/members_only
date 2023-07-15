1293167f6250f4a155c55ec8c02872a57f6dbab9
icarus2176 <ashe.kemuri@gmail.com>
2023-07-15 17:30:05 -0500

    add controllers

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root "posts#index"
+
+  resources :users, only: [:new, :create]
+  resources :posts, only: [:new, :create]
 end
